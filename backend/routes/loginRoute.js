1b330f92e79404bb797fbf732dc9e09572a5aa9e
Your name <Your name>
2024-10-07 17:01:16 +0530

    second commit

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require('../models/UserModel');
+const User  = require('../models/UserModel');
 
 router.post('/', async (req, res) => {
     const { name, phone, password } = req.body;
--- backend/routes/reservationRoute.js
+++ backend/routes/reservationRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Reservation } = require('../models/ReservationModel');
+const  Reservation  = require('../models/ReservationModel');
 
 router.post('/', async (req, res) => {
     const { name, contactNumber, numPersons, reservationDate, reservationTime } = req.body;
